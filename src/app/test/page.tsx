c62495de742e7f7f9297919821be94fcdeacd2dd
ArtHoxha <art@dioxconnect.com>
2024-05-04 19:22:16 +0200

    effect values updates

--- src/app/test/page.tsx
+++ src/app/test/page.tsx
@@ -4,8 +4,10 @@ import './page.css';
 const Page = () => {
   return (
     <main className="flex min-h-screen bg-black max-w-full overflow-x-hidden">
-      <div className="border border-red-500 p-10 h-[200px] w-[300px] rounded-lg mt-10 mx-auto">
-          <h3 className="text-white font-medium text-xl text-center"> SynTech is your future!</h3>
+      <div className="border border-blue-300 h-[200px] w-[300px] rounded-lg mt-10 mx-auto">
+          <h3 className="text-white font-medium text-xl text-center bg-red-500 p-10 relative z-10 rounded-lg">
+            SynTech is your future!
+          </h3>
       </div>
       <div id="blob"></div>
       <script src="script2.js"></script>
