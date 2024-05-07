f5dc89ef6dc26f9143e248979510083f43ccf31d
Art Hoxha <arthoxha34@gmail.com>
2024-05-07 23:43:18 +0200

    updated background animations

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,20 +2,25 @@ import type {Metadata} from "next";
 import "./globals.css";
 import Header from "@/components/header/header";
 import Footer from "@/components/footer/footer";
-import {ReactNode} from "react";
-
+import React, {ReactNode} from "react";
+import SynTransparent from "../../public/syn-transparent.png";
 export const metadata: Metadata = {
   title: "Syn-Tech",
   description: "Welcome to Syn-Tech!",
 };
 
-export default function RootLayout({children}: Readonly<{children: ReactNode}>) {
+export default function RootLayout({children}: Readonly<{ children: ReactNode }>) {
   return (
     <html lang="en">
-    <body className="bg-black">
+    <body className="bg-black overflow-x-hidden">
+      <div className="fixed top-0 left-0 w-full h-full z-[-1]"
+        style={{backgroundImage: `url(${SynTransparent.src})`}}
+      />
+      <div id="blob" />
       <Header/>
-      {children}
+        {children}
       <Footer/>
+      <script src="blob-script.js"></script>
     </body>
     </html>
   );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,8 @@
+import React from "react";
+
 const Home = () => {
   return (
-    <main className="flex min-h-screen bg-black max-w-full overflow-x-hidden">
-      <canvas id="starCanvas" className="absolute top-0 left-0"></canvas>
-      <script src="script.js"></script>
+    <main className="flex min-h-[5000px]">
     </main>
   );
 }
