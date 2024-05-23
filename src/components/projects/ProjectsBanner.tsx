016214e3de3889cd7314f6710517a23703abd783
Artim Gashi <73635267+gashiartim@users.noreply.github.com>
2024-05-23 14:18:17 +0200

    feat: Improve ProjectsBanner layout and add gradient to bottom
    The code changes include improving the layout of the ProjectsBanner component and adding a gradient to the bottom section. This enhances the visual appeal and overall design of the banner on the portfolio page.

--- src/components/projects/ProjectsBanner.tsx
+++ src/components/projects/ProjectsBanner.tsx
@@ -19,13 +19,13 @@ export const ProjectsBanner: FC<ProjectsBannerProps> = () => {
           wrapperClassNames="w-full h-full"
         />
       </div>
-      <div className="flex flex-col justify-between h-full w-full min-h-screen relative">
+      <div className="flex flex-col justify-between h-full w-full min-h-screen relative z-[2]">
         <div className="container flex items-center inset-x-0 mt-[187px] gap-x-5 justify-between">
           <div>
             <h1 className="text-[45px] leading-[62px] text-white w-full max-w-[522px]">
               Brand identity, design & development for luxury cruise booking
             </h1>
-            <div className="flex mt-[50px] group">
+            <div className="flex mt-[50px] group w-max">
               <Button to="/" variant="secondary">
                 See Live
               </Button>
@@ -40,7 +40,7 @@ export const ProjectsBanner: FC<ProjectsBannerProps> = () => {
         </div>
         <ProjectInformations />
       </div>
-      <div className="absolute inset-x-0 bottom-0 bg-gradient-to-t z-[2] from-black/80 h-[440px]"></div>
+      <div className="absolute inset-x-0 bottom-0 bg-gradient-to-t z-[1] from-black/80 h-[440px]"></div>
     </div>
   );
 };
