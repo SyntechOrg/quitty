65304055638e82c992634c6cd559ca721c7585a7
Art Hoxha <arthoxha34@gmail.com>
2024-05-23 21:23:53 +0200

    add hero section/background with scroll animation

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,77 @@
-const Home = () => (
-  <main className="flex min-h-[5000px]" />
-);
+"use client";
+import {motion, useScroll, useTransform} from "framer-motion";
+import {useRef} from "react";
+import {Icon, IconType} from "@/components/shared";
+
+const Home = () => {
+  const ref = useRef(null);
+  const {scrollY} = useScroll({
+    target: ref
+  });
+
+  const fullDisappear = useTransform(scrollY, [50, 200], [1, 0]);
+  const almostDisappear = useTransform(scrollY, [50, 300], [1, 0.3]);
+  const changeMaxWidth = useTransform(scrollY, [50, 300], [1232, 1600]);
+  const changeWidth = useTransform(scrollY, [50, 300], ["85%", "90%"]);
+
+  return (
+    <div ref={ref}>
+      <motion.div
+        style={{
+          maxWidth: changeMaxWidth,
+          width: changeWidth,
+        }}
+        className="fixed z-[-1] left-[50%] translate-x-[-50%] top-[50%] translate-y-[-50%]">
+        <motion.div
+          style={{
+            opacity: fullDisappear,
+          }}
+          className="absolute left-[50%] translate-x-[-50%] top-[50%] translate-y-[-50%]"
+        >
+          <Icon
+            icon={IconType.HERO_STAR}
+            className="relative w-[25vw] h-full max-w-[300px] max-h-[65vh]"
+          />
+        </motion.div>
+        <motion.div
+          style={{
+            opacity: almostDisappear,
+          }}
+          className="absolute top-[50%] translate-y-[-50%]">
+          <Icon
+            icon={IconType.HERO_ARROW_LEFT}
+            className="relative w-[25vw] h-full max-w-[300px] max-h-[65vh]"
+          />
+        </motion.div>
+        <motion.div
+          style={{
+            opacity: almostDisappear,
+          }}
+          className="absolute top-[50%] translate-y-[-50%] right-0">
+          <Icon
+            icon={IconType.HERO_ARROW_RIGHT}
+            className="relative w-[25vw] h-full max-w-[300px] max-h-[65vh]"
+          />
+        </motion.div>
+        <motion.div
+          style={{
+            opacity: fullDisappear,
+          }}
+          className="absolute left-[50%] translate-x-[-50%] top-[50%] translate-y-[-50%]"
+        >
+          <Icon
+            icon={IconType.SYNTECH_HERO_TEXT}
+            className="relative w-[75vw] max-w-[1200px] max-h-[65vh]"
+          />
+        </motion.div>
+      </motion.div>
+      <motion.div
+        className="max-h-[680px] h-screen flex items-center justify-center"
+      >
+        {/*test23test3*/}
+      </motion.div>
+    </div>
+  );
+}
 
 export default Home;
--- src/components/shared/icon/icon-types.ts
+++ src/components/shared/icon/icon-types.ts
@@ -3,4 +3,8 @@ export enum IconType {
   ARROW = "arrow",
   DIAMOND = "diamond",
   LOGO_BACKGROUND = "logo-background",
+  SYNTECH_HERO_TEXT = "syntech-hero-text",
+  HERO_STAR = "hero-star",
+  HERO_ARROW_LEFT = "hero-arrow-left",
+  HERO_ARROW_RIGHT = "hero-arrow-right",
 }
