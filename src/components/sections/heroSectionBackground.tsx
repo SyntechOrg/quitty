cc4b45145416fb071a3d913a997caf02a7586e0b
Art Hoxha <arthoxha34@gmail.com>
2024-05-28 12:07:55 +0200

    remove comment

--- src/components/sections/heroSectionBackground.tsx
+++ src/components/sections/heroSectionBackground.tsx
@@ -12,7 +12,7 @@ const HeroSectionBackground = () => {
   const fullDisappear = useTransform(scrollY, [40, 500], [1, 0]);
   const almostDisappear = useTransform(scrollY, [40, 500], [1, 0.3]);
   const changeMaxWidth = useTransform(scrollY, [40, 1000], [1179, 1600]);
-  const changeWidth = useTransform(scrollY, [40, 700], ["55vw", "95vw"]); // scrollYProgress
+  const changeWidth = useTransform(scrollY, [40, 700], ["55vw", "95vw"]);
 
   return (
     <div ref={ref} className={`min-h-[calc(100vh-106px)] md:min-h-[calc(100vh-114px)]`}>
