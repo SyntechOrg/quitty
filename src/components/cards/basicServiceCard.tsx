0ae3d6109a52a17098194e133321554856a80259
Art Hoxha <arthoxha34@gmail.com>
2024-05-28 13:15:17 +0200

    adjust service section cards

--- src/components/cards/basicServiceCard.tsx
+++ src/components/cards/basicServiceCard.tsx
@@ -1,9 +1,9 @@
-import React, {FC} from 'react';
+import React, {FC, ReactNode} from 'react';
 import Image, {StaticImageData} from "next/image";
 import {Button, Icon, IconType} from "@/components/shared";
 
 interface BasicServiceCardProps {
-  title: string,
+  title: ReactNode,
   description: string,
   image: StaticImageData,
   categories: string[],
@@ -14,34 +14,34 @@ const BasicServiceCard: FC<BasicServiceCardProps> = ({title, description, image,
   return (
     <div
       className="relative flex flex-col lg:flex-row justify-between max-w-[1300px] w-full bg-[#0F0F13] border border-gray
-      lg:px-[100px] lg:py-[60px] md:p-[60px] sm:p-[50px] px-0 py-8 rounded-[30px] overflow-clip"
+      lg:px-[90px] lg:py-[55px] md:p-[60px] sm:p-[50px] px-0 py-8 rounded-[30px] overflow-clip"
     >
-      <div className="w-11/12 lg:w-1/2 mx-auto max-sm:text-center">
-        <h4 className="lg:text-[50px] md:text-[36px] text-[24px]">
+      <div className="w-11/12 lg:w-[55%] mx-auto max-sm:text-center">
+        <h4 className="lg:text-[48px] md:text-[36px] text-[24px] leading-[1.16]">
           {title}
         </h4>
-        <p className="lg:mt-[45px] max-sm:text-[15px] md:mt-8 mt-5 leading-[1.5]">
+        <p className="lg:mt-[40px] max-sm:text-[15px] text-[17px] md:mt-8 mt-5 leading-[1.5] text-[#a6a6b1] max-w-[630px]">
           {description}
         </p>
-        <div className="flex flex-wrap items-center max-sm:justify-center gap-2.5 lg:mt-[100px] md:mt-[50px] mt-7">
+        <div className="flex flex-wrap items-center max-sm:justify-center gap-2 lg:mt-[100px] md:mt-[50px] mt-7">
           {categories.map((category, index) => (
             <div
               key={index}
-              className="rounded-3xl px-[30px] h-[40px] bg-[#15151b] border border-gray grid place-content-center w-fit"
+              className="rounded-3xl px-7 h-[40px] bg-[#15151b] border border-gray grid place-content-center w-fit"
             >
-              <span className="md:text-[17px] text-[15px] leading-[1]">
+              <span className="md:text-[16px] text-[15px] leading-[1]">
                 {category}
               </span>
             </div>
           ))}
         </div>
       </div>
-      <div className="relative w-full lg:w-1/2 max-lg:flex max-lg:flex-col-reverse">
+      <div className="relative w-full lg:w-[45%] max-lg:flex max-lg:flex-col-reverse">
         <Image
           src={image}
           alt="service"
-          className="lg:absolute relative md:top-[60px] sm:top-[50px] top-[40px] w-full h-full max-lg:max-w-[400px] max-lg:mx-auto
-          object-contain"
+          className="lg:absolute relative lg:top-[100px] md:top-[75px] sm:top-[65px] top-[45px] w-11/12 h-full max-w-[380px] max-h-[340px] right-0
+           max-lg:mx-auto object-contain"
         />
         <div className="group flex items-center lg:absolute lg:right-0 relative sm:ml-auto max-sm:mx-auto mt-10">
           <Button to="portfolio">Read More</Button>
