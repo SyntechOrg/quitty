0fabd3d6f438bad94e86265a37c1dde7ac163ab7
Art Hoxha <arthoxha34@gmail.com>
2024-05-29 10:12:35 +0200

    chore: added prettier mainly for tailwind classes formatting

--- src/components/sections/serviceCard.tsx
+++ src/components/sections/serviceCard.tsx
@@ -1,60 +1,68 @@
-import React, {FC, ReactNode} from 'react';
-import Image, {StaticImageData} from "next/image";
-import {Button, Icon, IconType} from "@/components/shared";
+import React, { FC, ReactNode } from 'react';
+import Image, { StaticImageData } from 'next/image';
+import { Button, Icon, IconType } from '@/components/shared';
 
 interface ServiceCardProps {
-  title: ReactNode,
-  description: string[],
-  image: StaticImageData,
-  FAQ: { question: string, answer: string }[],
-  href: string
+  title: ReactNode;
+  description: string[];
+  image: StaticImageData;
+  FAQ: { question: string; answer: string }[];
+  href: string;
 }
 
-const ServiceCard: FC<ServiceCardProps> = ({title, description, image, FAQ, href}) => {
+const ServiceCard: FC<ServiceCardProps> = ({
+  title,
+  description,
+  image,
+  FAQ,
+  href,
+}) => {
   return (
     <div
-      className="relative flex flex-col lg:flex-row justify-between max-w-[1300px] w-full bg-[#0F0F13] border border-gray
-      lg:px-[90px] lg:py-[55px] md:p-[60px] sm:p-[50px] px-0 py-8 rounded-[30px] overflow-clip"
+      className="relative flex w-full max-w-[1300px] flex-col justify-between overflow-clip rounded-[30px] border
+      border-gray bg-[#0F0F13] px-0 py-8 sm:p-[50px] md:p-[60px] lg:flex-row lg:px-[90px] lg:py-[55px]"
     >
-      <div className="w-11/12 lg:w-[46%] max-lg:mx-auto max-sm:text-center">
-        <h2 className="lg:text-[50px] md:text-[36px] text-[24px] leading-[1.33] lg:min-h-[260px]">
+      <div className="w-11/12 max-lg:mx-auto max-sm:text-center lg:w-[46%]">
+        <h2 className="text-[24px] leading-[1.33] md:text-[36px] lg:min-h-[260px] lg:text-[50px]">
           {title}
         </h2>
-        <p
-          className="lg:mt-[40px] max-sm:text-[15px] text-[17px] md:mt-8 mt-5 leading-[1.5] text-[#a6a6b1] max-w-[630px]">
+        <p className="mt-5 max-w-[630px] text-[17px] leading-[1.5] text-[#a6a6b1] max-sm:text-[15px] md:mt-8 lg:mt-[40px]">
           {description.map((desc, index) => (
             <span key={index}>
               {desc}
               {index !== description.length - 1 && (
                 <>
-                  <br/>
-                  <br/>
+                  <br />
+                  <br />
                 </>
               )}
             </span>
           ))}
         </p>
       </div>
-      <div className="relative w-11/12 lg:w-[46%] max-lg:mx-auto max-lg:flex flex-col-reverse lg:mt-0 mt-14 ">
-        <div className="lg:max-w-[350px] lg:max-h-[280px] max-w-[280px] max-h-[320px] mx-auto lg:absolute top-0 left-[-100px]
-        w-11/12 h-full max-lg:mx-auto lg:mb-0 md:mb-[-75px] sm:mb-[-60px] mb-[-45px] lg:mt-0 mt-14">
+      <div className="relative mt-14 w-11/12 flex-col-reverse max-lg:mx-auto max-lg:flex lg:mt-0 lg:w-[46%]">
+        <div
+          className="left-[-100px] top-0 mx-auto mb-[-45px] mt-14 h-full max-h-[320px] w-11/12
+          max-w-[280px] max-lg:mx-auto sm:mb-[-60px] md:mb-[-75px] lg:absolute lg:mb-0 lg:mt-0 lg:max-h-[280px] lg:max-w-[350px]"
+        >
           <Image
             src={image}
             alt="service"
-            className="w-full h-full object-contain"
+            className="h-full w-full object-contain"
           />
         </div>
         <div>
           <div
-            className="group flex items-center lg:absolute lg:right-0 lg:top-[-20px] relative w-fit sm:ml-auto max-sm:mx-auto mt-10"
+            className="group relative mt-10 flex w-fit items-center max-sm:mx-auto sm:ml-auto lg:absolute lg:right-0
+            lg:top-[-20px]"
           >
             <Button to="/portfolio">Choose service</Button>
             <Button to="/portfolio">
-              <Icon icon={IconType.ARROW}/>
+              <Icon icon={IconType.ARROW} />
             </Button>
           </div>
         </div>
-        <div className="w-full lg:mt-[310px] bg-red-950">
+        <div className="w-full bg-red-950 lg:mt-[310px]">
           questions and answers
         </div>
       </div>
@@ -63,4 +71,3 @@ const ServiceCard: FC<ServiceCardProps> = ({title, description, image, FAQ, href
 };
 
 export default ServiceCard;
-
