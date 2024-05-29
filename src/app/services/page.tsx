1ab1e8ee5398cd1491ee599b442ecc91b4b47828
Art Hoxha <arthoxha34@gmail.com>
2024-05-29 11:46:34 +0200

    task: added section title

--- src/app/services/page.tsx
+++ src/app/services/page.tsx
@@ -161,6 +161,9 @@ const ServicesPage = () => {
       />
       <div className="container mb-[60px] md:mb-[120px] lg:mb-[150px]">
         <div className="mt-[50px] space-y-[50px] md:mt-[80px] lg:mt-[100px]">
+          <h2 className="category-text text-center lg:mb-20 lg:text-[22px]">
+            WE ARE EXPERTS IN
+          </h2>
           {services.map((service, index) => (
             <ServiceCard
               key={index}
