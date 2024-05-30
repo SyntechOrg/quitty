2650ea7d668ede2e261e39019dde4d7900ce65b9
Art Hoxha <arthoxha34@gmail.com>
2024-05-30 18:45:19 +0200

    hotfix: fixed for deployment

--- src/components/contact-page/Banner.tsx
+++ src/components/contact-page/Banner.tsx
@@ -1,46 +1,44 @@
-import { type FC } from "react";
+import { type FC } from 'react';
 
 const GENERAL_INFO = [
   {
-    label: "PHONE NUMBER",
-    value: "+383 49 799 829",
+    label: 'PHONE NUMBER',
+    value: '+383 49 799 829',
   },
   {
-    label: "EMAIL ADDRESS",
-    value: "info@syn-tech.ch",
+    label: 'EMAIL ADDRESS',
+    value: 'info@syn-tech.ch',
   },
   {
-    label: "STUDIO ADDRESS",
-    value: "Strase Switzerland",
+    label: 'STUDIO ADDRESS',
+    value: 'Strase Switzerland',
   },
 ];
 
-type ContactPageBannerProps = {};
-
-export const ContactPageBanner: FC<ContactPageBannerProps> = () => {
+export const ContactPageBanner: FC = () => {
   return (
-    <div className="mt-[210px] container">
-      <div className="flex lg:justify-between lg:pr-[110px] flex-col lg:flex-row items-center justify-center lg:items-start text-center lg:text-left">
-        <div className="text-primary mt-[30px] text-[24px] tracking-[20%]">
+    <div className="container mt-[210px]">
+      <div className="flex flex-col items-center justify-center text-center lg:flex-row lg:items-start lg:justify-between lg:pr-[110px] lg:text-left">
+        <div className="mt-[30px] text-[24px] tracking-[20%] text-primary">
           GET IN TOUCH
         </div>
-        <h1 className="lg:text-[70px] text-[30px] leading-[40px] lg:leading-[80px] w-full max-w-[650px]">
-          Let's create something special together...
+        <h1 className="w-full max-w-[650px] text-[30px] leading-[40px] lg:text-[70px] lg:leading-[80px]">
+          Let&apos;s create something special together...
         </h1>
       </div>
-      <div className="flex lg:items-center justify-between mt-[161px] flex-col lg:flex-row gap-y-5 lg:gap-y-0">
+      <div className="mt-[161px] flex flex-col justify-between gap-y-5 lg:flex-row lg:items-center lg:gap-y-0">
         <p className="w-full max-w-[340px] text-sm leading-[30px]">
           Have a project or want to talk?
-          <br /> Here you'll find a few convenient <br />
+          <br /> Here you&apos;ll find a few convenient <br />
           ways to reach us.
         </p>
-        <ul className="flex lg:flex-row w-full justify-between flex-col">
+        <ul className="flex w-full flex-col justify-between lg:flex-row">
           {GENERAL_INFO.map((info) => (
             <li
               key={info.label}
-              className="pl-[30px] lg:pl-[60px] py-3 border-l border-l-[#3F3D45] last-of-type:border-r-[#3F3D45] lg:last-of-type:border-r lg:last-of-type:pr-[60px]"
+              className="border-l border-l-[#3F3D45] py-3 pl-[30px] last-of-type:border-r-[#3F3D45] lg:pl-[60px] lg:last-of-type:border-r lg:last-of-type:pr-[60px]"
             >
-              <div className="text-sm text-[#B0B0B0] font-bold mb-[30px]">
+              <div className="mb-[30px] text-sm font-bold text-[#B0B0B0]">
                 {info.label}
               </div>
               <div className="text-xl font-bold">{info.value}</div>
