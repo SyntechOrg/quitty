00a527e24f1e917a43e3d0d91cffb24f71a8968a
Art Hoxha <arthoxha34@gmail.com>
2024-05-30 15:21:55 +0200

    chore: minor adjustments to hero image, hero project informations texts, and refactoring

--- src/components/projects/index.ts
+++ src/components/projects/index.ts
@@ -1,4 +1,4 @@
-export { ProjectsBanner } from "./ProjectsBanner";
-export { ProjectInformations } from "./ProjectInformations";
-export { ProjectInteraction } from "./ProjectInteraction";
-export { ChallengeAndSolution } from "./ChallengeAndSolution";
+export { ProjectsBanner } from './ProjectsBanner';
+export { ProjectInformations } from './ProjectInformations';
+export { ProjectInteraction } from './ProjectInteraction';
+export { ChallengeAndSolution } from './ChallengeAndSolution';
