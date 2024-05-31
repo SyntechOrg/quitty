834876d2d38f2b06f7de36050db5ecf65f44a1db
Jon R <jon.r@monun.ch>
2024-05-31 22:27:09 +0200

    added first shared translation

--- src/components/sections/FeaturedWorksSection.tsx
+++ src/components/sections/FeaturedWorksSection.tsx
@@ -1,11 +1,15 @@
+"Use Client";
 import React from "react";
 import { Button, Icon, IconType } from "@/components/shared";
 import AllProjects, {
   AllProjectsHeader,
 } from "@/components/sections/AllProjects";
 import { FadeIn } from "../fade-in/FadeIn";
+import { useTranslations } from "next-intl";
 
 const FeaturedWorksSection = () => {
+  const t = useTranslations("Shared");
+
   return (
     <FadeIn>
       <AllProjectsHeader />
@@ -14,7 +18,7 @@ const FeaturedWorksSection = () => {
         data-aos="fade-up"
         className="mt-[40px] flex items-center justify-center md:hidden"
       >
-        <Button to="/portfolio">All Cases</Button>
+        <Button to="/portfolio">{t("All Cases")}</Button>
         <Button to="/portfolio">
           <Icon icon={IconType.ARROW} />
         </Button>
