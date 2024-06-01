be0c7dd1ffdd7851d7e0c06f68d389240db49f2a
Jon R <jon.r@monun.ch>
2024-06-01 17:33:23 +0200

    i did about us translations

--- src/components/cards/OverviewCard.tsx
+++ src/components/cards/OverviewCard.tsx
@@ -1,7 +1,9 @@
 import React from "react";
 import { Button, Icon, IconType } from "@/components/shared";
+import { useTranslations } from "next-intl";
 
 const OverviewCard = () => {
+  const t = useTranslations("About Us")
   return (
     <div
       data-aos="fade-up"
@@ -9,14 +11,13 @@ const OverviewCard = () => {
     >
       <div className="mx-auto w-11/12 max-w-[700px] text-center">
         <h4 className="mx-auto max-w-[600px] text-[24px] leading-[1.14] md:text-[32px] lg:text-[48px]">
-          Unlock 30 minutes to dream together
+          {t("About overview-title")}
         </h4>
         <p className="mt-6 text-[14px] font-light leading-[1.875] md:mt-10 md:text-[16px]">
-          Join us for a 30-minute conversation where we can explore your ideas,
-          collaborate on possibilities, and shape your vision into reality!
+          {t("About overview-text")}
         </p>
         <div className="group mx-auto mt-6 flex w-fit items-center md:mt-10 ">
-          <Button to="/contact">Book the journey</Button>
+          <Button to="/contact">{t("About overview-button")}</Button>
           <Button to="/contact">
             <Icon icon={IconType.ARROW} />
           </Button>
