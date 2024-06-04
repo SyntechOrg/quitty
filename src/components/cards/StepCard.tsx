94449360b61a8a222332f5bcb864f8ef043beeef
Art Hoxha <arthoxha34@gmail.com>
2024-06-04 14:22:35 +0200

    quick fix

--- src/components/cards/StepCard.tsx
+++ src/components/cards/StepCard.tsx
@@ -12,7 +12,7 @@ const StepCard: FC<TestimonialCardProps> = ({ title, description }) => {
   return (
     <div
       className="relative min-w-[70vw] rounded-[30px] border border-[#2C2A2E] bg-dark-gradient p-6 transition-transform duration-300 hover:scale-105
-      max-sm:flex max-sm:flex-col-reverse max-sm:items-center max-sm:justify-center max-sm:gap-4 max-sm:text-center sm:min-w-[400px] md:min-w-[560px] md:px-12 md:py-14"
+      max-sm:flex max-sm:flex-col max-sm:items-center max-sm:justify-center max-sm:gap-4 max-sm:text-center sm:min-w-[400px] md:min-w-[560px] md:px-12 md:py-14"
     >
       <p className="text-[15px] font-bold leading-[1.5] tracking-[0.02em] md:text-[18px]">
         {t(title)}
