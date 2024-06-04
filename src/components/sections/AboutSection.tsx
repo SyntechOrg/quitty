ebc8fb40ce2bd3a24a7911818c464bf17f2695a1
Art Hoxha <arthoxha34@gmail.com>
2024-06-04 13:28:21 +0200

    quickfix

--- src/components/sections/AboutSection.tsx
+++ src/components/sections/AboutSection.tsx
@@ -44,7 +44,7 @@ const AboutSection = () => {
         >
           {t("AboutUs text-1")}
         </p>
-        <div className="group mt-[20px] flex items-center max-lg:justify-center md:mt-[24px] lg:mt-[32px]">
+        <div className="group mt-[20px] w-fit flex items-center max-lg:justify-center md:mt-[24px] lg:mt-[32px]">
           <Button to={`/${localActive}/about`}>{t("AboutUs button-1")}</Button>
           <Button to={`/${localActive}/about`} className="btn2 px-[21px]">
             <Icon icon={IconType.ARROW} />
