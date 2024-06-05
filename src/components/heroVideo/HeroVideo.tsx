1559531b060bce9bb1380cb872e70d943a682852
Art Hoxha <arthoxha34@gmail.com>
2024-06-05 17:32:57 +0200

    prettier

--- src/components/heroVideo/HeroVideo.tsx
+++ src/components/heroVideo/HeroVideo.tsx
@@ -4,7 +4,7 @@ const HeroVideo = () => {
   return (
     <video
       src="/videos/syntech-about-video.mp4"
-      className="pointer-events-none max-h-[780px] w-full max-w-[1300px] pt-[90px] lg:pt-[114px] mix-blend-lighten lg:absolute lg:right-0 lg:top-0 lg:z-[-1] lg:w-[70%]"
+      className="pointer-events-none max-h-[780px] w-full max-w-[1300px] pt-[90px] mix-blend-lighten lg:absolute lg:right-0 lg:top-0 lg:z-[-1] lg:w-[70%] lg:pt-[114px]"
       loop
       muted
       autoPlay
