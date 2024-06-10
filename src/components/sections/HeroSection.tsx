e18a01a716d4a429196f345810ef42ff9e6dac78
Art Hoxha <arthoxha34@gmail.com>
2024-06-10 09:18:53 +0200

    chore

--- src/components/sections/HeroSection.tsx
+++ src/components/sections/HeroSection.tsx
@@ -8,7 +8,8 @@ const HeroSection = () => {
   return (
     <div
       id="hero-text"
-      className="container mx-auto flex min-h-screen w-10/12 max-w-[850px] items-center justify-center"
+      style={{ minHeight: "100dvh" || "100vh" }}
+      className="container mx-auto flex w-10/12 max-w-[850px] items-center justify-center"
     >
       <FadeIn className="h-fit">
         <h1 className="text-center text-[22px] leading-[1.66] sm:text-[36px] md:text-[42px] lg:text-[52px]">
