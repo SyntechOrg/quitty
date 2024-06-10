ae79f0a1793d8386d3d87453a3cbf1995f057101
Art Hoxha <arthoxha34@gmail.com>
2024-06-10 09:13:38 +0200

    chore

--- src/components/sections/HeroSection.tsx
+++ src/components/sections/HeroSection.tsx
@@ -6,7 +6,10 @@ const HeroSection = () => {
   const t = useTranslations("Home");
 
   return (
-    <div className="container mx-auto flex min-h-screen w-10/12 max-w-[850px] items-center justify-center">
+    <div
+      style={{ minHeight: "100dvh" }}
+      className="container mx-auto flex min-h-screen w-10/12 max-w-[850px] items-center justify-center"
+    >
       <FadeIn className="h-fit">
         <h1 className="text-center text-[22px] leading-[1.66] sm:text-[36px] md:text-[42px] lg:text-[52px]">
           {t("HeroSectionTitle")}
