dcc8c09bd98e7f61723a5661ed13583e1692dd8b
Jxni1 <jonramadani303@gmail.com>
2024-11-18 14:43:37 +0100

    pages and translations

---
+++ src/app/[locale]/impressum/page.tsx
@@ -0,0 +1,31 @@
+import { useTranslations } from "next-intl";
+import React from "react";
+
+const ImpressumPage = () =>{
+    const t = useTranslations('Impressum');
+
+    return(
+        <div className="container lg:mt-[200px] mt-[150px]">
+        <h1 className="font-bold lg:text-[60px] text-[50px]">{t('Title')}</h1>
+        <h5 className="lg:text-[18px] text-[16px]">{t('UnderText')}</h5>
+
+        <p className="lg:text-[16px] text-[14px] lg:mt-[150px] mt-[75px]">{t('Paragraph1-1')}</p>
+        <p className="lg:text-[16px] text-[14px] lg:mt-[25px] mt-[15px]">{t('Paragraph1-2')}</p>
+        <p className="lg:text-[16px] text-[14px] lg:mt-[25px] mt-[15px]">{t('Paragraph1-3')}</p>
+
+        <h2 className="lg:text-[38px] text-[30px] lg:mt-[25px] mt-[15px] max-w-[700px] font-bold">{t('UnderTitle')}</h2>
+    
+        <p className="lg:text-[16px] text-[14px] lg:mt-[25px] mt-[15px]">{t('Paragraph1-4')}</p>
+        <p className="lg:text-[16px] text-[14px] lg:mt-[25px] mt-[15px]">{t('Paragraph1-5')}</p>
+        <p className="lg:text-[16px] text-[14px] lg:mt-[25px] mt-[15px]">{t('Paragraph1-6')}</p>
+        <p className="lg:text-[16px] text-[14px] lg:mt-[25px] mt-[15px]">{t('Paragraph1-7')}</p>
+
+        <h3 className="lg:text-[30px] text-[25px] lg:mt-[25px] mt-[15px]">{t('Paragraph-title')}</h3>
+
+        <p className="lg:text-[16px] text-[14px] lg:mt-[25px] mt-[15px]">{t('Paragraph1-8')}</p>
+        <p className="lg:text-[16px] text-[14px] lg:mt-[25px] mt-[15px]">{t('Paragraph1-9')}</p>
+    </div>
+    )
+}
+
+export default ImpressumPage;
