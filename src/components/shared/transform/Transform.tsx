df9481b6cc3f33dc07f7f1838fe4b953444f8ee3
Jxni1 <jonramadani303@gmail.com>
2024-11-25 11:16:55 +0100

    small improvements

--- src/components/shared/transform/Transform.tsx
+++ src/components/shared/transform/Transform.tsx
@@ -2,19 +2,25 @@ import React from "react";
 import Image from "next/image";
 import phone from "public/assets/images/TransformPhone.png";
 import flame from "public/assets/images/flameIcon.png";
-import apple from "public/assets/images/apple-icon.png"
-import play from "public/assets/images/google-play-icon.png"
+import apple from "public/assets/images/apple-logo.png"
+import play from "public/assets/images/google-play-logo.png";
 import { useTranslations } from "next-intl";
+import bgBlur from "public/assets/images/bgBlur.png";
 
 
 const Transform = () => {
     const t = useTranslations("Shared");
     return(
         <>
-        <div className="lg:mt-[120px] mt-[80px] container flex lg:flex-row flex-col lg:justify-center justify-end lg:items-end items-center lg:gap-[5%] bg-[#E6F2F4] rounded-[80px] lg:pt-[100px] h-[auto] pt-[50px]">
-            <div className="flex flex-col lg:gap-[30px] gap-[17px] mb-[10%] lg:items-start items-center ">
-                <h1 className="lg:max-w-[550px] lg:text-[50px] text-[39px] font-medium lg:leading-[64px] leading-[50px] lg:text-left text-center">{t('TransformTitle')}</h1>
+        <div className="relative border-l-2 border-t-2 border-l-[#b6d0f8] border-t-[#b6d0f8] p-4 lg:mt-[120px] mt-[80px] container flex lg:flex-row flex-col lg:justify-center justify-end lg:items-end items-center lg:gap-[5%] bg-[#E6F2F4] rounded-[80px] lg:pt-[100px] h-[auto] pt-[50px]">
+            <Image 
+                src={bgBlur}
+                alt=""
+                className="absolute lg:w-[88%] w-[100%] h-[100%] top-0 right-0"
+            />
 
+            <div className="z-10 flex flex-col lg:gap-[30px] gap-[17px] mb-[10%] lg:items-start items-center ">
+                <h1 className="lg:max-w-[550px] text-[36px] font-medium lg:leading-[64px] leading-[50px] lg:text-left text-center">{t('TransformTitle')}</h1>
                 <div className="flex flex-col lg:gap-[25px] gap-[20px] w-fit">
                     <div className="flex lg:justify-start justify-center items-start gap-[10px] w-fit">
                         <Image 
@@ -22,7 +28,7 @@ const Transform = () => {
                         alt=""
                         className="w-[22px] mt-[5px]"
                         />
-                        <p className="lg:text-[20px] text-[18px]">{t('TransformP1')}</p>
+                        <p className="text-[16px]">{t('TransformP1')}</p>
                     </div>
                     <div className="flex lg:justify-start justify-center items-start gap-[10px] w-fit">
                         <Image 
@@ -30,7 +36,7 @@ const Transform = () => {
                         alt=""
                         className="w-[22px] mt-[5px]"
                         />
-                        <p className="lg:text-[20px] text-[18px]">{t('TransformP2')}</p>
+                        <p className="text-[16px]">{t('TransformP2')}</p>
                     </div>
                     <div className="flex lg:justify-start justify-center items-start gap-[10px] w-fit">
                         <Image 
@@ -38,24 +44,24 @@ const Transform = () => {
                         alt=""
                         className="w-[22px] mt-[5px]"
                         />
-                        <p className="lg:text-[20px] text-[18px]">{t('TransformP3')}</p>
+                        <p className="text-[16px]">{t('TransformP3')}</p>
                     </div>
                 </div>
 
                 <div className="flex lg:justify-start lg:flex-row flex-col justify-center items-center gap-[20px] w-fit">
-                    <button className="w-[200px] h-[60px] border border-color-[#00C9A5] rounded-full flex justify-center items-center gap-[10px]">
+                    <button className="border border-primary hover:bg-primary duration-150 w-[200px] h-[60px] border border-color-[#00C9A5] rounded-full flex justify-center items-center gap-[10px]">
                     <Image 
                         src={apple}
                         alt=""
-                        className=""
+                        className="w-[30px]"
                         />
                         App Store
                     </button>
-                    <button className="w-[200px] h-[60px] border border-color-[#00C9A5] rounded-full flex justify-center items-center gap-[10px]">
+                    <button className="border border-primary hover:bg-primary duration-150 w-[200px] h-[60px] border border-color-[#00C9A5] rounded-full flex justify-center items-center gap-[10px]">
                     <Image 
                         src={play}
                         alt=""
-                        className=""
+                        className="w-[30px]"
                         />
                         Google Play
                     </button>
@@ -65,7 +71,7 @@ const Transform = () => {
             <Image 
             src={phone}
             alt="Tranform Phone"
-            className="lg:w-[39%] w-[300px]" 
+            className="lg:w-[39%] w-[300px] z-10" 
             />
         </div>
         </>
