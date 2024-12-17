d72d99acbb6ea9bb20fb54b9d470644731d2bf13
arthoxha <arthoxha34@gmail.com>
2024-12-17 22:58:58 +0100

    list

---
+++ src/components/sections/feedback/FeedbackStacked.tsx
@@ -0,0 +1,212 @@
+"use client";
+import {
+  ResponsiveContainer,
+  StackedCarousel,
+  StackedCarouselSlideProps,
+} from "react-stacked-center-carousel";
+import React from "react";
+import "./FeedbackStacked.css";
+import Image from "next/image";
+import FeedbackBackground from "../../../../public/assets/images/feedback-background.png";
+import { Icon, IconType } from "@/components/shared";
+import useIsLargeScreen from "@/hooks/useIsLargeScreen";
+import AvatarPlaceholder from "../../../../public/assets/images/avatar-placeholder.png";
+
+const CONTENT = [
+  {
+    bgColor: "#323CD2",
+    avatar: AvatarPlaceholder,
+    name: "Retailer X",
+    city: "Switzerland",
+    stars: 5,
+    review:
+      "“Seit der Integration von Quitty haben wir die Papierkosten um 35 % reduziert und einen Anstieg der Wiederholungskäufe um 25 % verzeichnet.”",
+    date: "20 Mar, 2024",
+  },
+  {
+    bgColor: "#00C9A5",
+    avatar: AvatarPlaceholder,
+    name: "Retailer X",
+    city: "Switzerland",
+    stars: 4,
+    review:
+      "“Seit der Integration von Quitty haben wir die Papierkosten um 35 % reduziert und einen Anstieg der Wiederholungskäufe um 25 % verzeichnet.”",
+    date: "21 Mar, 2024",
+  },
+  {
+    bgColor: "#3F7CF3",
+    avatar: AvatarPlaceholder,
+    name: "Retailer X",
+    city: "Switzerland",
+    stars: 5,
+    review:
+      "“Since integrating Quitty, we’ve reduced paper costs by 35% and seen a 25% increase in repeat purchases.”",
+    date: "22 Mar, 2024",
+  },
+];
+
+export const FeedbackStacked = () => {
+  const ref = React.useRef<any>();
+  const isLargeScreen = useIsLargeScreen();
+
+  const slideWidth = isLargeScreen ? 420 : 295;
+  const slideFadeDistance = isLargeScreen ? 0.85 : 0.55;
+
+  return (
+    <div className="relative my-[80px] flex items-center justify-center py-[80px] lg:my-[150px] lg:py-[120px]">
+      <div className="absolute z-[-1] mx-auto h-full w-screen max-w-[1660px] overflow-clip">
+        <div className="h-full w-full bg-black/5 blur-sm">
+          <Image
+            src={FeedbackBackground}
+            alt="background"
+            className="h-full w-full object-cover"
+          />
+        </div>
+      </div>
+      <div className="h-full w-full">
+        <div className="mx-auto max-w-[650px]">
+          <p className="text-center text-[18px] font-medium leading-[1.5] text-primary">
+            Erfolgsgeschichten
+          </p>
+          <h6 className="leading:text-[48px] mt-5 text-center text-[32px] font-medium leading-[1.5] text-text">
+            Sehen Sie, wie wir führenden Einzelhändlern zum Erfolg verholfen
+            haben.
+          </h6>
+        </div>
+        <div className="relative mx-auto mt-12 h-full min-h-[300px] w-full max-w-[800px]">
+          <ResponsiveContainer
+            carouselRef={ref}
+            render={(width, carouselRef) => {
+              return (
+                <StackedCarousel
+                  ref={carouselRef}
+                  slideComponent={Slide}
+                  slideWidth={slideWidth}
+                  carouselWidth={width}
+                  data={CONTENT}
+                  height={340}
+                  maxVisibleSlide={5}
+                  customScales={[1, 0.95, 0.85, 0.3]}
+                  fadeDistance={slideFadeDistance}
+                  transitionTime={500}
+                />
+              );
+            }}
+          />
+          <button
+            className="absolute left-[-25px] top-1/2 z-[200] flex size-[40px] translate-y-[-50%] items-center justify-center rounded-full border
+           border-[#7a8889] hover:opacity-70 active:opacity-100 lg:size-[50px]"
+            onClick={() => ref.current?.goBack()}
+          >
+            <Icon
+              icon={IconType.CAROUSEL_RIGHT}
+              className="h-full w-full max-w-[11px] object-contain"
+            />
+          </button>
+          <button
+            className="absolute right-[-25px] top-1/2 z-[200] flex size-[40px] translate-y-[-50%] items-center justify-center rounded-full border
+           border-[#7a8889] hover:opacity-70 active:opacity-100 lg:size-[50px]"
+            onClick={() => ref.current?.goNext()}
+          >
+            <Icon
+              icon={IconType.CAROUSEL_LEFT}
+              className="h-full w-full max-w-[11px] object-contain"
+            />
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+// eslint-disable-next-line react/display-name
+const Slide = React.memo(function (props: StackedCarouselSlideProps) {
+  const { dataIndex, isCenterSlide, swipeTo, slideIndex } = props;
+  const [loadDelay, setLoadDelay] = React.useState<any>();
+  const [removeDelay, setRemoveDelay] = React.useState<any>();
+  const [loaded, setLoaded] = React.useState(false);
+  React.useEffect(() => {
+    if (isCenterSlide) {
+      clearTimeout(removeDelay);
+      setLoadDelay(setTimeout(() => setLoaded(true), 1));
+    } else {
+      clearTimeout(loadDelay);
+      if (loaded) setRemoveDelay(setTimeout(() => setLoaded(false), 1));
+    }
+  }, [isCenterSlide]);
+
+  React.useEffect(() => () => {
+    clearTimeout(removeDelay);
+    clearTimeout(loadDelay);
+  });
+
+  const card = CONTENT[dataIndex];
+
+  return (
+    <div
+      className="twitch-card overflow-clip rounded-[52px]"
+      draggable={false}
+      style={{
+        backgroundColor: card.bgColor,
+      }}
+    >
+      <div
+        className={`cover h-full w-full ${isCenterSlide && loaded ? "off" : "on"}`}
+      >
+        <div
+          className="card-overlay h-full w-full"
+          onClick={() => {
+            if (!isCenterSlide) swipeTo(slideIndex);
+          }}
+        />
+      </div>
+      {/*{loaded && (*/}
+      <div className="h-full w-full p-6 lg:p-7">
+        <div className="flex items-center gap-4">
+          <div className="h-full max-h-[55px] w-full max-w-[55px]">
+            <Image
+              src={card.avatar}
+              alt="avatar image"
+              className="h-full w-full object-contain"
+            />
+          </div>
+          <div>
+            <p className="text-[20px] font-medium leading-[1.3] text-white lg:text-[24px]">
+              {card.name}
+            </p>
+            <p className="text-[15px] font-medium leading-[1.4] text-white lg:text-[16px]">
+              {card.city}
+            </p>
+          </div>
+        </div>
+        <div className="mt-4 flex gap-2 lg:mt-5">
+          {Array.from({ length: card.stars }).map((_, index) => (
+            <Icon
+              key={index}
+              icon={IconType.STAR}
+              className="h-full w-full max-w-[24px] object-contain lg:max-w-[32px]"
+            />
+          ))}
+          {Array.from({ length: 5 - card.stars }).map((_, index) => (
+            <Icon
+              key={index}
+              icon={IconType.STAR}
+              className="h-full w-full max-w-[24px] object-contain opacity-50 lg:max-w-[32px]"
+            />
+          ))}
+        </div>
+        <div className="mt-4 lg:mt-5">
+          <p className="text-[16px] font-medium leading-[1.4] text-white">
+            {card.review}
+          </p>
+          <p className="mt-2 text-[15px] font-medium leading-[1.4] text-white lg:mt-3 lg:text-[16px]">
+            {card.date}
+          </p>
+        </div>
+      </div>
+      {/*)}*/}
+    </div>
+  );
+});
+
+export default FeedbackStacked;
