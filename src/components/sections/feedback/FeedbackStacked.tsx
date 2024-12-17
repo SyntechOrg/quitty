a63b46961133f8acd18f6eca524d70efa98e8355
arthoxha <arthoxha34@gmail.com>
2024-12-17 23:02:48 +0100

    disabled user select

--- src/components/sections/feedback/FeedbackStacked.tsx
+++ src/components/sections/feedback/FeedbackStacked.tsx
@@ -73,7 +73,7 @@ export const FeedbackStacked = () => {
             haben.
           </h6>
         </div>
-        <div className="relative mx-auto mt-12 h-full min-h-[300px] w-full max-w-[800px]">
+        <div className="select-none relative mx-auto mt-12 h-full min-h-[300px] w-full max-w-[800px]">
           <ResponsiveContainer
             carouselRef={ref}
             render={(width, carouselRef) => {
