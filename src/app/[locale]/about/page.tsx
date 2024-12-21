261923746b71906e8dcd1667327a9a32e5d83929
arthoxha <arthoxha34@gmail.com>
2024-12-21 14:00:21 +0100

    add Impact Section and adjust other sections

--- src/app/[locale]/about/page.tsx
+++ src/app/[locale]/about/page.tsx
@@ -5,6 +5,8 @@ import Mission from "@/components/sections/mission/Mission";
 import Partners from "@/components/shared/partners/Partners";
 import PackagesSection from "@/components/sections/PackagesSection/PackagesSection";
 import FeedbackStacked from "@/components/sections/feedback/FeedbackStacked";
+import CircularCarousel from "@/components/CircularCarousel/CircularCarousel";
+import ImpactSection from "@/components/ImpactSection/ImpactSection";
 
 export const metadata: Metadata = {
   title: "Quitty | About Us",
@@ -21,6 +23,7 @@ const AboutPage = () => {
         text="Heading-text1"
       />
       <Mission />
+      <ImpactSection />
       <FeedbackStacked />
       <PackagesSection />
     </div>
