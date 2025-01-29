abfc06e0a159b451e0ba2c6a7165d79b62cf9ebe
arthoxha <arthoxha34@gmail.com>
2025-01-29 17:57:39 +0100

    prod

--- src/app/[locale]/data/page.tsx
+++ src/app/[locale]/data/page.tsx
@@ -40,7 +40,6 @@ const DataPage = () => {
       text: "contactCardtt3",
     },
   ];
-
   return (
     <FadeIn className="mt-[120px] md:mt-[150px] lg:mt-[200px]">
       <h1 className="container text-[35px] md:text-[50px] lg:text-[60px]">
