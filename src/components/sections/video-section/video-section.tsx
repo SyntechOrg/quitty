e57b1eb90e2d8f1cc16900dfb116b08f1cce95a5
Jxni1 <jonramadani303@gmail.com>
2026-02-27 13:28:52 +0100

    video update

--- src/components/sections/video-section/video-section.tsx
+++ src/components/sections/video-section/video-section.tsx
@@ -4,18 +4,15 @@ import { FadeIn } from "@/components/fade-in/FadeIn";
 const VideoSection = () => {
   return (
     <FadeIn className="mt-32 lg:mt-52">
-      <div style={{ padding: "55.6% 0 0 0", position: "relative" }}>
-        <iframe
-          src="https://player.vimeo.com/video/1168773089?badge=0&autopause=0&player_id=0&app_id=58479"
-          frameBorder="0"
-          allow="autoplay; fullscreen; picture-in-picture; clipboard-write; encrypted-media; web-share"
-          referrerPolicy="strict-origin-when-cross-origin"
-          style={{ position: "absolute", top: 0, left: 0, width: "100%", height: "100%" }}
-          className="rounded-[40px]"
-          title="quitty draft 3(1)"
-        ></iframe>
-      </div>
-      <script src="https://player.vimeo.com/api/player.js"></script>
+      <iframe
+        className="z-10 mx-auto aspect-video w-full rounded-[40px] lg:w-full"
+        src="https://player.vimeo.com/video/1168835735?badge=0&autopause=0&player_id=0&app_id=58479"
+        frameBorder="0"
+        allow="autoplay; fullscreen; picture-in-picture; clipboard-write; encrypted-media; web-share"
+        referrerPolicy="strict-origin-when-cross-origin"
+        allowFullScreen
+        title="quitty draft 7(1)"
+      ></iframe>
     </FadeIn>
   );
 };
