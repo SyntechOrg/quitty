697b46948a4abba16371a66c09bb09be9125ab4f
Jxni1 <jonramadani303@gmail.com>
2026-03-05 12:23:36 +0100

    video cahnge

--- src/components/sections/video-section/video-section.tsx
+++ src/components/sections/video-section/video-section.tsx
@@ -6,7 +6,7 @@ const VideoSection = () => {
     <FadeIn className="mt-32 lg:mt-52">
       <iframe
         className="z-10 mx-auto aspect-video w-full rounded-[40px] lg:w-full"
-        src="https://player.vimeo.com/video/1168835735?badge=0&autopause=0&player_id=0&app_id=58479"
+        src="https://player.vimeo.com/video/1170629815?badge=0&autopause=0&player_id=0&app_id=58479"
         frameBorder="0"
         allow="autoplay; fullscreen; picture-in-picture; clipboard-write; encrypted-media; web-share"
         referrerPolicy="strict-origin-when-cross-origin"
